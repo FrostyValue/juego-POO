67ce2548053d23a58d8a58118fd77d6cece137cf
FrostyValue <saturmendezc@gmail.com>
2025-02-19 17:49:05 +0100

    Score 1 asteorid * 10 points added and timer added.

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -10,14 +10,18 @@ class Game {
     this.character = null;
     this.coins = [];
     this.score = 0;
+    this.astGame = 0;
     this.level = 1;
-    this.asteroids = this.introduceAsteroids();
+    this.asteroids = this.level;
+    //Se añaden 2 segundos para compensar el delay en pararse el cronometro de nivel
+    this.timeLeft = 17;
     this.mute = false;
 
     this.createScenary();
     this.addEvents();
     this.checkCollisions();
     this.updateScore();
+    this.startTimer();
     this.screenMuButtonEvent(this.muteButton);
     this.screenResButEvent(this.resetButton);
   }
@@ -41,23 +45,24 @@ class Game {
 
     window.addEventListener("resize", () => {
       game.character.updatePosition();
-      game.coins.forEach(coin => coin.updatePosition());
+      game.coins.forEach((coin) => coin.updatePosition());
     });
-
   }
 
-  // Logica de colisiones modificada para que no entre en conflicto con la animacion, si no se marca como cayendo y se deja 
+  // Logica de colisiones modificada para que no entre en conflicto con la animacion, si no se marca como cayendo y se deja
   // la version anterior no contabiliza algunos asteroides, creandose un bug en el juego que impide su finalización.
   checkCollisions() {
     setInterval(() => {
       this.coins = this.coins.filter((coin) => {
-        if (this.character.collisionWith(coin) && !coin.isFalling) { //Se comprueba que la moneda no esta cayendo a parte de la colision.
-          coin.isFalling = true; //Aqui se marca que esta cayendo
+        if (this.character.collisionWith(coin) && !coin.isFalling) {
+          coin.isFalling = true;
           coin.animateFall();
           setTimeout(() => {
             this.container.removeChild(coin.element);
-            this.score++;
+            this.score += 10;
+            this.astGame++;
             this.updateScore();
+
             this.finishGame();
           }, 2000);
           return false;
@@ -86,16 +91,18 @@ class Game {
     }
 
     if (event.key == "m") {
-        this.muteGame();
+      this.muteGame();
     }
   }
 
   updateScore() {
-    this.scoreElement.textContent = `${this.score} asteroids broken of ${this.asteroids}.`;
+    this.scoreElement.textContent = `Score: ${this.score}`;
   }
 
   finishGame() {
-    if (this.score === this.asteroids) {
+    if (this.astGame === this.asteroids) {
+      clearInterval(this.timer);
+
       if (this.level < 200) {
         this.level++;
         if (this.level % 25 === 0) {
@@ -111,42 +118,73 @@ class Game {
   }
 
   resetGame(fullReset) {
-    this.containerText.textContent = `Restarting game...`;
     setTimeout(() => {
+      this.containerText.textContent = `Starting level ${this.level}...`;
       this.character.jumpSound(true);
-      this.container.querySelectorAll(".personaje, .moneda").forEach((el) => el.remove());
-      this.score = 0;
-      this.asteroids = this.introduceAsteroids();
-      if (fullReset) this.level = 1;
+      this.container
+        .querySelectorAll(".personaje, .moneda")
+        .forEach((el) => el.remove());
+      this.asteroids = this.level;
+      this.astGame = 0;
+      if (fullReset) {
+        this.level = 1;
+        this.containerText.textContent = `Restarting game...`;
+        {
+        }
+        this.asteroids = this.level;
+        this.score = 0;
+      }
       this.updateScore();
       this.createScenary();
+
       setTimeout(() => {
         this.containerText.textContent = "";
+        this.startTimer();
       }, 1500);
     }, 1550);
   }
 
   muteGame() {
     if (this.mute === true) {
-        this.mute = false;
-        this.muteButton.textContent = "";
-        this.muteButton.textContent = "Mute"
+      this.mute = false;
+      this.muteButton.textContent = "";
+      this.muteButton.textContent = "Mute";
+    } else {
+      this.mute = true;
+      this.muteButton.textContent = "";
+      this.muteButton.textContent = "Unmute";
     }
-
-    else {
-        this.mute = true;
-        this.muteButton.textContent = "";
-        this.muteButton.textContent = "Unmute"
-    }
-
-  }
-
-  introduceAsteroids() {
-    return this.level; // Asteroides igual al nivel actual
   }
 
   updateScore() {
-    this.scoreElement.textContent = `Level ${this.level} - ${this.score} asteroids broken of ${this.asteroids}.`;
+    // Se restan 2 segundos al tiempo reflejado para que no se vea el delay
+    this.scoreElement.textContent = `Level ${this.level} - Score: ${
+      this.score
+    } - Time: ${Math.max(0, (this.timeLeft - 2)).toFixed(1)}s`;
+  }
+
+  startTimer() {
+    clearInterval(this.timer);
+    this.timeLeft = 17 + (this.level - 1) * 0.1;
+    this.updateScore();
+
+    this.timer = setInterval(() => {
+      // Restar 0.1 al tiempo restante
+      this.timeLeft = Math.max(0, this.timeLeft - 0.1);
+      this.updateScore();
+
+      if (this.timeLeft <= 0) {
+        clearInterval(this.timer);
+        this.handleTimeOut();
+      }
+    }, 100); // Se actualiza cada milisegundo
+  }
+
+  handleTimeOut() {
+    this.containerText.textContent = "Time's up! You Lose.";
+    setTimeout(() => {
+      this.resetGame(true);
+    }, 2000);
   }
 
   endSound(mute) {
@@ -158,7 +196,6 @@ class Game {
     audio.currentTime = 0;
     audio.play();
   }
-  
 }
 
 class Entity {
@@ -181,7 +218,12 @@ class Character extends Entity {
     const container = document.getElementById("game-container");
     const containerWidth = container.clientWidth;
     const containerHeight = container.clientHeight;
-    super(containerWidth * 0.5, containerHeight * 0.85, containerWidth * 0.05, containerHeight * 0.1);
+    super(
+      containerWidth * 0.5,
+      containerHeight * 0.85,
+      containerWidth * 0.05,
+      containerHeight * 0.1
+    );
     this.container = container;
     this.speed = containerWidth * 0.02;
     this.jumping = false;
@@ -193,7 +235,8 @@ class Character extends Entity {
     const containerWidth = this.container.clientWidth; // Obtener el ancho actualizado
 
     if (event.key === "ArrowRight" || event.key === "d") {
-      if (this.x + this.speed < containerWidth - this.width) this.x += this.speed; // Evitar que salga del contenedor
+      if (this.x + this.speed < containerWidth - this.width)
+        this.x += this.speed; // Evitar que salga del contenedor
     } else if (event.key === "ArrowLeft" || event.key === "a") {
       if (this.x - this.speed > 0) this.x -= this.speed;
     } else if (event.key === "ArrowUp" || event.key === "w") {
@@ -236,7 +279,6 @@ class Character extends Entity {
     }, 20);
   }
 
-
   fallCollision() {
     const gravityInterval = setInterval(() => {
       if (this.y < 520) {
@@ -258,7 +300,6 @@ class Character extends Entity {
       this.y < object.y + object.height &&
       this.y + this.height > object.y
     );
-
   }
 
   jumpSound(end) {
@@ -275,7 +316,6 @@ class Character extends Entity {
   increaseSpeed() {
     this.speed *= 1.1; // Aumenta la velocidad en un 10%
   }
-  
 }
 
 class Coin extends Entity {
@@ -285,8 +325,8 @@ class Coin extends Entity {
     const containerHeight = container.clientHeight;
 
     super(
-      Math.random() * (containerWidth * 0.9) + (containerWidth * 0.05),
-      Math.random() * (containerHeight * 0.8) + (containerHeight * 0.1),
+      Math.random() * (containerWidth * 0.9) + containerWidth * 0.05,
+      Math.random() * (containerHeight * 0.8) + containerHeight * 0.1,
       containerWidth * 0.05,
       containerHeight * 0.1
     );
